2666d377df4ee0d876ef6509fb2cd9ac249bd20d
srappltd <abhaygautam626039@gmail.com>
2024-05-17 17:09:41 +0530

    first commit

---
+++ src/Utils/JwtToken.jsx
@@ -0,0 +1,12 @@
+export const SetJwtToken = (token)=>{
+    localStorage.setItem("token",token)
+}
+
+
+export const isAuthoticated = ()=>{
+    return !!localStorage.getItem("token")
+}
+
+export const isAdmin = () =>{
+    return localStorage.getItem("admin_token") == "ADMIN"
+}
